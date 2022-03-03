f7b850cd3008025148b11ed3d563c277b08fd241
Venialis <nfs41738@mail.ru>
2022-03-03 21:43:50 +0300

    write a sait

--- taskmaster/myhome/models.py
+++ taskmaster/myhome/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length=255)
+    pages_number = models.IntegerField(default=100)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    description = models.TextField(null=True, blank=True)
